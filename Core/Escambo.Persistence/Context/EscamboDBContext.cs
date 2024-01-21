469a5438e28ccfd1f4c02679a529a6e6015e971f
Daniel Oliveira <danieloliveira.s558@gmail.com>
2024-01-21 08:57:47 -0300

    Alteração

--- Core/Escambo.Persistence/Context/EscamboDBContext.cs
+++ Core/Escambo.Persistence/Context/EscamboDBContext.cs
@@ -13,20 +13,4 @@ public class EscamboDBContext: DbContext
     public DbSet<Message> Messages { get; set; }
     public DbSet<Avaluation> Avaluations { get; set; }
 
-
-     #region Required
-
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder){
-        var connectionString = "server=localhost;user=root;database=escambo;port=3306;password=123456";
-        optionsBuilder.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
-    }
-    protected override void OnModelCreating(ModelBuilder modelBuilder)
-    {
-        base.OnModelCreating(modelBuilder);
-
-        modelBuilder.Entity<User>().ToTable("Users");
-    }
-
-
-    #endregion
 }
