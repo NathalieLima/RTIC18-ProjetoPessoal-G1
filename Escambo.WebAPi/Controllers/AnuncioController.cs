d13f533a0b531091c4c64b336a8423fcb74bca67
Caua-Clemente <cauaclementelima@gmail.com>
2024-01-26 12:23:36 -0300

    Subindo prototipo do controller-Anuncio

---
+++ Escambo.WebAPi/Controllers/AnuncioController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Escambo.WebAPI.Infra.Data.Interfaces;
+using Escambo.WebAPI.Model;
+
+namespace TechMed.WebAPI.Controllers;
+
+[ApiController]
+[Route("/api/v0.1/")]
+public class AnuncioController : ControllerBase
+{
+   [HttpGet("anuncios")]
+   public IActionResult Get()
+   {
+      return NoContent();
+   }
+
+   [HttpGet("anuncio/{id}")]
+   public IActionResult GetById(int id)
+   {
+      return NoContent();
+   }
+
+   [HttpPost("anuncio")]
+   public IActionResult Post([FromBody] Anuncio anuncio)
+   {
+      return NoContent();
+   }
+
+   [HttpPut("anuncio/{id}")]
+   public IActionResult Put(int id, [FromBody] Anuncio anuncio)
+   {
+      return NoContent();
+   }
+
+   [HttpDelete("anuncio/{id}")]
+   public IActionResult Delete(int id)
+   {
+      return NoContent();
+   }
+   [HttpGet("anuncios/{id}/prestacoes")]
+   public IActionResult GetAllPrestacoes(int id)
+   {
+      return NoContent();
+   }
+}
---
+++ Escambo.WebAPi/Infra.Data/Interfaces/IAnuncioCollection.cs
@@ -0,0 +1,9 @@
+﻿using Escambo.WebAPI.Model;
+
+namespace Escambo.WebAPI.Infra.Data.Interfaces;
+
+public interface IAnuncioCollection : IBaseCollection<Anuncio>
+{
+    void Update(int id, Anuncio obj);
+    void AnuncioGetAllPrestacoes();
+}
