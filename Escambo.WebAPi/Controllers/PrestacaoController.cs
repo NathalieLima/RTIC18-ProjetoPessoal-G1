999f572a5ad2748ce09a0243e489d2a28e6b5126
Caua-Clemente <cauaclementelima@gmail.com>
2024-01-28 20:41:58 -0300

    Criando controller de prestacao

---
+++ Escambo.WebAPi/Controllers/PrestacaoController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Escambo.WebAPI.Infra.Data.Interfaces;
+using Escambo.WebAPI.Model;
+
+namespace TechMed.WebAPI.Controllers;
+
+[ApiController]
+[Route("/api/v0.1/")]
+public class PrestacaoController : ControllerBase
+{
+   [HttpGet("prestacoes")]
+   public IActionResult GetAll()
+   {
+      return NoContent();
+   }
+
+   [HttpGet("prestacao/{id}")]
+   public IActionResult GetById(int id)
+   {
+      return NoContent();
+   }
+
+   [HttpPost("prestacao")]
+   public IActionResult Post()
+   {
+      return NoContent();
+   }
+
+   [HttpPut("prestacao/{id}")]
+   public IActionResult Put(int id)
+   {
+      return NoContent();
+   }
+
+   [HttpDelete("prestacao/{id}")]
+   public IActionResult Delete(int id)
+   {
+      return NoContent();
+   }
+   [HttpGet("prestacao/{id}/avaliacoes")]
+   public IActionResult GetAllAvaliacoes(int id)
+   {
+      return NoContent();
+   }
+}
---
+++ Escambo.WebAPi/Infra.Data/Interfaces/IPrestacaoCollection.cs
@@ -0,0 +1,8 @@
+﻿using Escambo.WebAPI.Model;
+
+namespace Escambo.WebAPI.Infra.Data.Interfaces;
+public interface IPrestacaoCollection : IBaseCollection<PrestacaoServico>
+{  
+    void Update(int id, PrestacaoServico obj);
+    ICollection<Avaliacao> PrestacaoGetAllAvaliacoes();
+}
