334f2fb6a6e02dd136f08c2242ba5f127d009638
Lorena Andrade <lorenaandrade@gmail.com>
2024-01-28 15:19:36 -0300

    alteracoes entidade conversas

--- Escambo.WebAPi/Model/Conversa.cs
+++ Escambo.WebAPi/Model/Conversa.cs
@@ -5,13 +5,8 @@ public class Conversa
 {
 
     public int ConversaId { get; set; }
-    public DateTime DataHoraInicio { get; set; } //hora de incio
-    public DateTime DataHoraFim { get; set; } //hora de enceramento
-
-    // public int DestinatarioId { get; set; } //destinatario
-    // public required Usuario Destinatario { get; set; }
-    // public int RemetenteId { get; set; } //remetente
-    // public required Usuario Remetente { get; set; }
+    public DateTime DataHoraInicio { get; set; } 
+    public DateTime DataHoraFim { get; set; } 
 
     ICollection<Mensagem>? Mensagens { get; set; }
     public ICollection<ConversasHasUsuarios>? ConversasHasUsuarios { get; set; }
