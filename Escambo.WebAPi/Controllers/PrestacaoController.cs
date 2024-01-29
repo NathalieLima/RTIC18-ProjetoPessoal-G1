a810d52e6a64d7964f68f42258ccf7da7b6c16b8
Lorena Andrade <lorenaandrade@gmail.com>
2024-01-28 21:49:14 -0300

    Incrementando a prestacao de servico

--- Escambo.WebAPi/Controllers/PrestacaoController.cs
+++ Escambo.WebAPi/Controllers/PrestacaoController.cs
@@ -11,30 +11,84 @@ public class PrestacaoController : ControllerBase
    [HttpGet("prestacoes")]
    public IActionResult GetAll()
    {
-      return NoContent();
+     
+
+        var usuarioPrestador = new Usuario
+        {
+            UsuarioId = 1,
+            Nome = "Prestador 1"
+        };
+
+        var usuarioContratante = new Usuario
+        {
+            UsuarioId = 2,
+            Nome = "Contratante 1"
+        };
+
+        var prestacoes =Enumerable.Range(1, 5).Select(index => new PrestacaoServico
+        {
+            AnuncioId = index,
+            Status = 0,//Aceito
+            DataInicio = DateTime.Now,
+            ContratanteId = usuarioContratante.UsuarioId,
+            Contratante = usuarioContratante,
+            PrestadorId = usuarioPrestador.UsuarioId,
+            Prestador = usuarioPrestador
+        })
+          .ToArray();
+        return Ok(prestacoes);
    }
 
    [HttpGet("prestacao/{id}")]
    public IActionResult GetById(int id)
    {
-      return NoContent();
+        var usuarioPrestador = new Usuario
+        {
+            UsuarioId = 1,
+            Nome = "Prestador 1"
+        };
+
+        var usuarioContratante = new Usuario
+        {
+            UsuarioId = 2,
+            Nome = "Contratante 1"
+        };
+
+        var prestacao = new PrestacaoServico
+        {
+            PrestacaoId = id,
+            AnuncioId = 1,
+            Status = 0,//Aceito
+            DataInicio = DateTime.Now,
+            ContratanteId = usuarioContratante.UsuarioId,
+            Contratante = usuarioContratante,
+            PrestadorId = usuarioPrestador.UsuarioId,
+            Prestador = usuarioPrestador
+        };
+        return Ok(prestacao);
    }
 
    [HttpPost("prestacao")]
-   public IActionResult Post()
+   public IActionResult Post([FromBody] PrestacaoServico prestacao)
    {
-      return NoContent();
+      return CreatedAtAction(nameof(GetById), new { id = 1 }, prestacao);
    }
 
    [HttpPut("prestacao/{id}")]
-   public IActionResult Put(int id)
+   public IActionResult Put(int id, [FromBody] PrestacaoServico prestacao)
    {
+      
+      //TODO: Buscar uma Prestacao de servico pelo id e atualizar os dados
+      // caso não encontre, retorna NotFound()
       return NoContent();
    }
 
    [HttpDelete("prestacao/{id}")]
    public IActionResult Delete(int id)
    {
+      
+      //TODO: Buscar uma Prestacao de servico pelo id e apagar
+      // caso não encontre, retorna NotFound()
       return NoContent();
    }
    [HttpGet("prestacao/{id}/avaliacoes")]
