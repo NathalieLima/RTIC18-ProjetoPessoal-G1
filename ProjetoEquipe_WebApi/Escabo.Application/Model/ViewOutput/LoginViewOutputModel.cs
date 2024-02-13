f29151ba5a652a839762b12813e470e6a8aa5eb5
Daniel Oliveira <danieloliveira.s558@gmail.com>
2024-02-12 22:04:52 -0300

    altualizando

--- ProjetoEquipe_WebApi/Escabo.Application/Model/ViewOutput/LoginViewOutputModel.cs
+++
@@ -1,6 +0,0 @@
-namespace Escambo.Application.Model.ViewOutput;
-public class LoginViewOutputModel
-{
-    public int LoginId{get;set;}
-    public string? Email {get;set;}
-}
--- ProjetoEquipe_WebApi/Escambo.Domain/Entities/Endereco.cs
+++
@@ -1,8 +0,0 @@
-namespace Escambo.Domain.Entities;
-public class Endereco{
-    public string? Rua { get; set; }
-    public string ? Bairro { get; set; }
-    public string? Cidade { get; set; }
-    public string? NumeroCasa { get; set; }
-    public string? CEP {get;set;}
-}
